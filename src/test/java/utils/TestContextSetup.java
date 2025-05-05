c0d09489e64060b11739e60ce5f5859e8277270c
Sean Winston <seanwinston@gmail.com>
2025-05-05 17:56:40 +0100

    feat: add pageObjectManager and genericUtils to the constructor of TestContextSetup

--- src/test/java/utils/TestContextSetup.java
+++ src/test/java/utils/TestContextSetup.java
@@ -1,10 +1,18 @@
 package utils;
 
+import pageobjects.PageObjectManager;
+
+import java.io.IOException;
+
 public class TestContextSetup {
 
     public TestBase testBase;
+    public PageObjectManager pageObjectManager;
+    public GenericUtils genericUtils;
 
-    public TestContextSetup () {
+    public TestContextSetup () throws IOException {
         testBase = new TestBase();
+        pageObjectManager = new PageObjectManager(testBase.webDriverManager());
+        genericUtils = new GenericUtils(testBase.webDriverManager());
     }
 }
