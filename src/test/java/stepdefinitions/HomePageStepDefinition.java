e40be8d35bb6e7018551b91860bcbba28b4bfad4
Sean Winston <seanwinston@gmail.com>
2025-05-06 15:06:43 +0100

    feat: add methods to click register page button

--- src/test/java/stepdefinitions/HomePageStepDefinition.java
+++ src/test/java/stepdefinitions/HomePageStepDefinition.java
@@ -4,21 +4,30 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.testng.Assert;
 import pageobjects.HomePage;
+import pageobjects.LoginPage;
 import utils.TestContextSetup;
 
 public class HomePageStepDefinition {
 
     TestContextSetup testContextSetup;
     HomePage homePage;
+    LoginPage loginPage;
 
     public HomePageStepDefinition(TestContextSetup testContextSetup) {
         this.testContextSetup = testContextSetup;
         this.homePage = testContextSetup.pageObjectManager.getHomePage();
+        this.loginPage = testContextSetup.pageObjectManager.getLoginPage();
     }
 
     @Given("I am on the Login page")
     public void i_am_on_the_login_page() {
         homePage.clickLoginPageButton();
+    }
+
+    @Given("I am on the register page")
+    public void i_am_on_the_register_page() {
+        homePage.clickLoginPageButton();
+        loginPage.clickRegisterPageButton();
     }
 
     @Then("I should be redirected to the Home page")
