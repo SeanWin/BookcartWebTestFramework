11471b6f89451d92cb4563a01610c116dbcde487
Sean Winston <seanwinston@gmail.com>
2025-05-06 15:11:49 +0100

    feat: add assertions for redirection to login page and for account being created

--- src/test/java/stepdefinitions/LoginPageStepDefinition.java
+++ src/test/java/stepdefinitions/LoginPageStepDefinition.java
@@ -1,6 +1,8 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 import pageobjects.LoginPage;
 import utils.TestContextSetup;
 
@@ -27,6 +29,22 @@ public class LoginPageStepDefinition {
     @When("I click the Login button")
     public void i_click_the_login_button() {
         loginPage.clickLoginButton();
+    }
+
+    @Then("I should be redirected to the Login page")
+    public void i_should_be_redirected_to_the_login_page() {
+        boolean titleMatches = testContextSetup.genericUtils.waitForTitle("Login", 3);
+        Assert.assertTrue(titleMatches);
+    }
+
+    @Then("verify the account has been created with login")
+    public void verify_the_account_has_been_created_with_login() throws InterruptedException {
+        Thread.sleep(2000);
+        i_enter_into_the_username_field(testContextSetup.generatedUsername);
+        i_enter_into_the_password_field(testContextSetup.password);
+        i_click_the_login_button();
+        boolean titleMatches = testContextSetup.genericUtils.waitForTitle("Home", 3);
+        Assert.assertTrue(titleMatches);
     }
 
 }
