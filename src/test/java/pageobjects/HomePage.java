9b39f5d8e46780bcd179b436e70dac811b017c44
Sean Winston <seanwinston@gmail.com>
2025-05-08 11:51:50 +0100

    feat: add methodto click logout button and checkif login button is present

--- src/test/java/pageobjects/HomePage.java
+++ src/test/java/pageobjects/HomePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
     By loginPageButton = By.xpath("//button[.//span[text()=' Login ']]");
     By profileUsername = By.xpath("//a[contains(@class, 'mat-mdc-menu-trigger')]//span[contains(@class, 'mdc-button__label')]/span");
+    By logoutButton = By.xpath("//span[text()='Logout']");
 
     public WebDriver driver;
 
@@ -19,5 +20,17 @@ public class HomePage {
 
     public String getProfileUsername() {
         return driver.findElement(profileUsername).getText();
+    }
+
+    public void clickProfileUsername() {
+        driver.findElement(profileUsername).click();
+    }
+
+    public void clickLogoutButton() {
+        driver.findElement(logoutButton).click();
+    }
+
+    public boolean isLoginButtonPagePresent() {
+        return !driver.findElements(loginPageButton).isEmpty();
     }
 }
