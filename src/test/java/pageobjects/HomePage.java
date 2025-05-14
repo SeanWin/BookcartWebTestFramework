90a7077185090fdd65b25780d29a8e2c3c6948e3
Sean Winston <seanwinston@gmail.com>
2025-05-14 12:23:32 +0100

    feat: add methods to click categories and assert book num and titles and if cart button present

--- src/test/java/pageobjects/HomePage.java
+++ src/test/java/pageobjects/HomePage.java
@@ -2,6 +2,10 @@ package pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePage {
     By loginPageButton = By.xpath("//button[.//span[text()=' Login ']]");
@@ -11,6 +15,12 @@ public class HomePage {
     By firstAutocompleteOption = By.cssSelector("mat-option[role='option']");
     By firstBookCoverImage = By.cssSelector("img[alt='Book cover image']");
     By autoCompletePanel = By.id("mat-autocomplete-0");
+    By biographyCategory = By.xpath("//mat-list-item//span[contains(text(), 'Biography')]");
+    By fictionCategory = By.xpath("//mat-list-item//span[contains(text(), 'Fiction')]");
+    By mysteryCategory = By.xpath("//mat-list-item//span[contains(text(), 'Mystery')]");
+    By books = By.cssSelector("app-book-card");
+    By title = By.cssSelector("div.card-title strong");
+    By addToCartButton = By.xpath(".//button[.//span[contains(normalize-space(), 'Add to Cart')]]");
 
     public WebDriver driver;
 
@@ -50,4 +60,41 @@ public class HomePage {
     public boolean isHidden() {
         return driver.findElement(autoCompletePanel).getAttribute("class").contains("mat-mdc-autocomplete-hidden");
     }
+
+    public void clickBiographyCategory() {
+        driver.findElement(biographyCategory).click();
+    }
+
+    public void clickFictionCategory() {
+        driver.findElement(fictionCategory).click();
+    }
+
+    public void clickMysteryCategory() {
+        driver.findElement(mysteryCategory).click();
+    }
+
+    public int countBooks() {
+        return driver.findElements(books).size();
+    }
+
+    public List<String> getBookTitles() throws InterruptedException {
+        Thread.sleep(1000);
+        List<String> bookTitles = new ArrayList<>();
+        List<WebElement> bookCards = driver.findElements(books);
+
+        for (WebElement card : bookCards) {
+            bookTitles.add(card.findElement(title).getText().trim());
+        }
+        return bookTitles;
+    }
+
+        public boolean isAddToCartButtonPresentForAllBooks() {
+            List<WebElement> bookCards = driver.findElements(books);
+            for (WebElement card : bookCards) {
+                if (!card.findElement(addToCartButton).isDisplayed()) {
+                    return false;
+                }
+            }
+            return true;
+        }
 }
--- src/test/java/stepdefinitions/HomePageStepDefinition.java
+++ src/test/java/stepdefinitions/HomePageStepDefinition.java
@@ -2,9 +2,14 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 import pageobjects.HomePage;
 import utils.TestContextSetup;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
 
 public class HomePageStepDefinition {
 
@@ -32,6 +37,21 @@ public class HomePageStepDefinition {
         homePage.clickBook();
     }
 
+    @When("I click the Biography category button")
+    public void i_click_the_biography_category_button() {
+        homePage.clickBiographyCategory();
+    }
+
+    @When("I click the Fiction category button")
+    public void i_click_the_fiction_category_button() {
+       homePage.clickFictionCategory();
+    }
+
+    @When("I click the Mystery category button")
+    public void i_click_the_mystery_category_button() {
+        homePage.clickMysteryCategory();
+    }
+
     @Then("I should be redirected to the Home page")
     public void i_should_be_redirected_to_the_home_page() {
         boolean titleMatches = testContextSetup.genericUtils.waitForTitle("Home", 3);
@@ -53,5 +73,20 @@ public class HomePageStepDefinition {
     public void no_results_are_displayed_in_the_drop_down_menu() {
         System.out.println(homePage.isHidden());
         Assert.assertTrue(homePage.isHidden());
+    }
+
+    @Then("{int} books are displayed")
+    public void books_are_displayed(int expectedBooksCount) {
+        Assert.assertEquals(homePage.countBooks(), expectedBooksCount);
+    }
+
+    @Then("^the books have the following titles:$")
+    public void the_books_have_the_following_titles(List<String> testData) throws InterruptedException {
+        MatcherAssert.assertThat(homePage.getBookTitles(), is(testData));
+    }
+
+    @Then("all books have an Add to Cart button")
+    public void all_books_have_an_add_to_cart_button() {
+        Assert.assertTrue(homePage.isAddToCartButtonPresentForAllBooks());
     }
 }
