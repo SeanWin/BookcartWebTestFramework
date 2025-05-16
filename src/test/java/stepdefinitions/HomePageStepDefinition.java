03c5fcc537e3e2e7a16d14bfc248af1aba790dde
Sean Winston <seanwinston@gmail.com>
2025-05-16 16:17:18 +0100

    feat: add step to click cart button

--- src/test/java/stepdefinitions/HomePageStepDefinition.java
+++ src/test/java/stepdefinitions/HomePageStepDefinition.java
@@ -82,6 +82,11 @@ public class HomePageStepDefinition {
         homePage.clickAddToCartButton();
     }
 
+    @When("I press the cart icon")
+    public void iPressTheCartIcon() {
+        homePage.clickCartButton();
+    }
+
     @Then("I should be redirected to the Home page")
     public void i_should_be_redirected_to_the_home_page() {
         boolean titleMatches = testContextSetup.genericUtils.waitForTitle("Home", 3);
