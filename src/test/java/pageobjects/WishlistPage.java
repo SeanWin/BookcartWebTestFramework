9bbd7607e1c02daf52f7e2c1a883b1772a6f7619
Sean Winston <seanwinston@gmail.com>
2025-05-20 15:23:47 +0100

    feat: add methods to get toast message and click remove from wishlist button for a book based on title

--- src/test/java/pageobjects/WishlistPage.java
+++ src/test/java/pageobjects/WishlistPage.java
@@ -2,10 +2,18 @@ package pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class WishlistPage {
     By clearWishlistButton = By.xpath("//button[.//span[text()=' Clear Wishlist ']]");
     By title = By.cssSelector("td.mat-column-title a");
+    By toastLocator = By.cssSelector(".mat-mdc-snack-bar-label");
+    String bookRow = "//tr[.//td[contains(@class,'mat-column-title')]//a[normalize-space()='%s']]";
+    By removeFromWishlistButton = By.xpath(".//button[.//span[normalize-space()='Remove from Wishlist']]");
 
     public WebDriver driver;
 
@@ -19,5 +27,19 @@ public class WishlistPage {
 
     public String getTitle() {
         return driver.findElement(title).getText();
+    }
+
+    public String getToastMessage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement toast = wait.until(ExpectedConditions.visibilityOfElementLocated(toastLocator));
+        return toast.getText().trim();
+    }
+
+    private By bookRowLocator(String title) {
+        return By.xpath(String.format(bookRow, title));
+    }
+
+    public void clickRemoveFromWishlistButtonForBook(String title) {
+        driver.findElement(bookRowLocator(title)).findElement(removeFromWishlistButton).click();
     }
 }
