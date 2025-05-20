7f7ddd7d624887f827f6388cfb8e604b6f153142
Sean Winston <seanwinston@gmail.com>
2025-05-20 12:27:28 +0100

    feat: create WishlistPage step defs with step to validate wishlist page title matches home page

---
+++ src/test/java/stepdefinitions/WishlistPageStepDefinition.java
@@ -0,0 +1,26 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pageobjects.HomePage;
+import pageobjects.WishlistPage;
+import utils.TestContextSetup;
+
+public class WishlistPageStepDefinition {
+
+    TestContextSetup testContextSetup;
+    HomePage homePage;
+    WishlistPage wishlistPage;
+
+    public WishlistPageStepDefinition(TestContextSetup testContextSetup) {
+        this.testContextSetup = testContextSetup;
+        this.homePage = testContextSetup.pageObjectManager.getHomePage();
+        this.wishlistPage = testContextSetup.pageObjectManager.getWishlistPage();
+    }
+
+    @Then("validate that the book title in the wishlist page matches with home page")
+    public void validateThatTheBookTitleInTheWishlistPageMatchesWithHomePage() {
+        homePage.clickWishlistPageButton();
+        Assert.assertEquals(testContextSetup.homePageBookTitle, wishlistPage.getTitle());
+    }
+}
